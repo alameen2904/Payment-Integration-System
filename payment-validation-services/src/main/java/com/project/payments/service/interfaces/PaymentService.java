af6303fc6d8c23d5eb459e94d616e1f9bb4242f0
alameen-2911 <itzalameen2004@gmail.com>
2026-05-11 16:56:33 +0530

    Remove extra microservices from config branch

--- payment-validation-services/src/main/java/com/project/payments/service/interfaces/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package com.project.payments.service.interfaces;
-
-import com.project.payments.pojo.PaymentRequest;
-
-public interface PaymentService {
-    String validateAndCreatePayment(PaymentRequest paymentRequest);
-}
--- stripe-provider-services/src/main/java/com/project/payments/service/interfaces/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.project.payments.service.interfaces;
-
-import com.project.payments.pojo.CreatePaymentReq;
-import com.project.payments.pojo.PaymentResponse;
-
-public interface PaymentService {
-
-    public PaymentResponse createPayment(CreatePaymentReq createPaymentReq);
-}
