bcff70ed0fa6dcab96e11d95f654ce0f3ec104f9
alameen-2911 <itzalameen2004@gmail.com>
2026-05-15 11:24:27 +0530

    add config server service

---
+++ payment-config-servers/src/test/java/comproject/payments/PaymentConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package comproject.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.payments.PaymentConfigServerApplication;
+
+@SpringBootTest(classes = PaymentConfigServerApplication.class) 
+class PaymentConfigServerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
