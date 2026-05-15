d111396e88f1b9d7f5e806dcb8bc62c1366d6eb7
alameen-2911 <itzalameen2004@gmail.com>
2026-05-15 11:30:21 +0530

    add stripe provider microservice

---
+++ stripe-provider-services/src/main/java/com/project/payments/stripe/StripeError.java
@@ -0,0 +1,14 @@
+package com.project.payments.stripe;
+
+
+import lombok.Data;
+
+@Data
+public class StripeError {
+	
+	private String code;
+    private String message;
+    private String param;
+    private String type;
+
+}
